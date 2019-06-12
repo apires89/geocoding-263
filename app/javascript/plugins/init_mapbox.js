9fd069781d9ca1b43a169630370c8c1b3bf670c8
Andre Pires <andrecabralpires@gmail.com>
2019-06-12 14:16:19 +0200

    updated mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,6 +5,8 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
+  // layers
+
   // fit map to markers
   const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -16,10 +18,80 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/apires89/cjpb0eog5a2tx2spj2qhqytwz'
+      style: 'mapbox://styles/apires89/cjpb0eog5a2tx2spj2qhqytwz',
+      pitch: 60, // pitch in degrees
+      bearing: -60 // bearing in degrees
     });
     // mapbox search bar
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
+    map.addControl(new MapboxGeocoder({ mapboxgl: mapboxgl, accessToken: mapboxgl.accessToken }));
+
+    //navigation controls
+
+    map.addControl(new mapboxgl.NavigationControl());
+// layers --- 3D Buildings
+    map.on('load', function() {
+// Insert the layer beneath any symbol layer.
+var layers = map.getStyle().layers;
+
+var labelLayerId;
+for (var i = 0; i < layers.length; i++) {
+if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+labelLayerId = layers[i].id;
+break;
+}
+}
+
+map.addLayer({
+'id': '3d-buildings',
+'source': 'composite',
+'source-layer': 'building',
+'filter': ['==', 'extrude', 'true'],
+'type': 'fill-extrusion',
+'minzoom': 15,
+'paint': {
+'fill-extrusion-color': '#aaa',
+
+// use an 'interpolate' expression to add a smooth transition effect to the
+// buildings as the user zooms in
+'fill-extrusion-height': [
+"interpolate", ["linear"], ["zoom"],
+15, 0,
+15.05, ["get", "height"]
+],
+'fill-extrusion-base': [
+"interpolate", ["linear"], ["zoom"],
+15, 0,
+15.05, ["get", "min_height"]
+],
+'fill-extrusion-opacity': .6
+}
+}, labelLayerId);
+});
+
+// DRONE
+
+var url = 'https://wanderdrone.appspot.com/';
+map.on('load', function () {
+map.loadImage('https://cdn3.iconfinder.com/data/icons/airport-collection/100/9-512.png', function(error, image) {
+if (error) throw error;
+map.addImage('cat', image);
+window.setInterval(function() {
+map.getSource('drone').setData(url);
+}, 2000);
+
+map.addSource('drone', { type: 'geojson', data: url });
+map.addLayer({
+"id": "drone",
+"type": "symbol",
+"source": "drone",
+"layout": {
+"icon-image": "cat",
+"icon-size": 0.025
+}
+});
+});
+});
+
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
@@ -31,5 +103,8 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
   }
 };
+
+
+
 
 export { initMapbox };
